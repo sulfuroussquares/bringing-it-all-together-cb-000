08cfe658fc6b3726ff9d1bd0e3e66ba24648cdca
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-11-20 17:41:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,6 +12,14 @@ def id
 end
 
 def self.create_table
+  sql = <<-SQL 
+  CREATE TABLE dogs(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+  )
+  SQL
+  DB[:conn].execute(sql)
 end
 
 
